6484a4ffc4a65e7f29c859a6a47692f502fb425f
chrehall68 <chrehall68@gmail.com>
2025-08-22 14:45:26 -0700

    get cookies

---
+++ background.js
@@ -0,0 +1,18 @@
+chrome.runtime.onMessage.addListener(async (request, sender, reply) => {
+  async function getCookies(domian, name) {
+    return await chrome.cookies.get({ url: domian, name: name });
+  }
+  let cookie1 = await getCookies(
+    "https://sjsu.collegescheduler.com",
+    "__RequestVerificationToken"
+  );
+  let cookie2 = await getCookies(
+    "https://sjsu.collegescheduler.com",
+    " .AspNet.Cookies"
+  );
+  console.log(cookie1);
+  console.log(cookie2);
+  console.log("hi there");
+
+  return true;
+});
--- popup.js
+++ popup.js
@@ -1,1 +1,10 @@
-console.log('This is a popup!');
+console.log("This is a popup!");
+console.log("Hi there!!!");
+console.log("This is my first extension");
+
+// try accessing the cookies for our website
+// __RequestVerificationToken
+// .AspNet.Cookies
+// both belong to sjsu.collegescheduler.com
+
+chrome.runtime.sendMessage({ greeting: "hello" });
