5a3c824c168b771f7f3cff7c17a4de7ebc8dac5d
chrehall68 <chrehall68@gmail.com>
2025-09-01 13:00:34 -0700

    add tailwind

--- src/popup/App.tsx
+++ src/popup/App.tsx
@@ -1,22 +1,27 @@
-import crxLogo from '@/assets/crx.svg'
-import reactLogo from '@/assets/react.svg'
-import viteLogo from '@/assets/vite.svg'
-import HelloWorld from '@/components/HelloWorld'
-import './App.css'
+import crxLogo from "@/assets/crx.svg";
+import reactLogo from "@/assets/react.svg";
+import viteLogo from "@/assets/vite.svg";
+import HelloWorld from "@/components/HelloWorld";
 
 export default function App() {
   return (
     <div>
-      <a href="https://vite.dev" target="_blank" rel="noreferrer">
-        <img src={viteLogo} className="logo" alt="Vite logo" />
-      </a>
-      <a href="https://reactjs.org/" target="_blank" rel="noreferrer">
-        <img src={reactLogo} className="logo react" alt="React logo" />
-      </a>
-      <a href="https://crxjs.dev/vite-plugin" target="_blank" rel="noreferrer">
-        <img src={crxLogo} className="logo crx" alt="crx logo" />
-      </a>
+      <div className="flex flex-row gap-4">
+        <a href="https://vite.dev" target="_blank" rel="noreferrer">
+          <img src={viteLogo} className="w-24 h-24" alt="Vite logo" />
+        </a>
+        <a href="https://reactjs.org/" target="_blank" rel="noreferrer">
+          <img src={reactLogo} className="w-24 h-24" alt="React logo" />
+        </a>
+        <a
+          href="https://crxjs.dev/vite-plugin"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img src={crxLogo} className="w-24 h-24" alt="crx logo" />
+        </a>
+      </div>
       <HelloWorld msg="Vite + React + CRXJS" />
     </div>
-  )
+  );
 }
