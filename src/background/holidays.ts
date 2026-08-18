637bc0d2dbc50ec33061a1f54b9d10d2a95d8b45
s-gobo <chingyeungkwok@gmail.com>
2026-08-17 20:46:18 -0700

    formats into ics kinda
    because it wants the ics formatted time, without the timezone info strangely enough

--- src/background/holidays.ts
+++ src/background/holidays.ts
@@ -16,10 +16,31 @@ const DATES = [
   "270402", // spring break v
 ];
 
+/**
+ * formats dates into a calendar-readable format for holiday exculsion
+ * @param startDateTime the start time of the event
+ * @returns a list of comma-sepearted dates as YYYYMMDDTHHMMSS with the time as the start time of the event
+ */
 export function exdatesCSV(startDateTime: string): string {
   return DATES.map(
-    date => processDate(
-      processDateZeroTime(date), // makes the date a proper datetime (with a time of 0)
-      extractTime(startDateTime)) // uses the start time of the event
+    date => convertToICSDate(
+      processDate(
+        processDateZeroTime(date), // makes the date a proper datetime (with a time of 0)
+        extractTime(startDateTime) // uses the start time of the event
+      )
+    )
   ).join(",");
+}
+/**
+ * basically the same as ./ics.ts/convertToICSDate but without the time zone info
+ * @param dateTimeString a date as YYYY-MM-DDTHH:MM:SS-HH:MM
+ * @returns the same date but as YYYYMMDDTHHMMSS
+ */
+function convertToICSDate(dateTimeString: string): string {
+  // strip the -08:00 or -07:00 from the end
+  // that's the last 6 characters
+  let time = dateTimeString.substring(0, dateTimeString.length - 6);
+  // get rid of the - and :
+  time = time.replaceAll("-", "").replaceAll(":", "");
+  return time;
 }
